63db663d770b6ef12e9b2308d0aed4139c78f94f
ardaerdogdu <101050579+ardaerdogdu1905@users.noreply.github.com>
2023-07-17 01:56:05 +0300

    Rock-Paper-Scissors
    a rock-paper-scissors game that can be played as many games as we want against the computer on the console

---
+++ GameAlgo.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    public class GameAlgo
+    {
+
+        private static readonly Random random = new Random();
+
+        public void Start()
+        {
+            try
+            {
+                Console.WriteLine("~Welcome to Rock Paper Scissors Game~");
+                Console.WriteLine("Press 's' to start");
+
+                string start = Console.ReadLine().ToLower();
+
+                if (start == "s")
+                {
+                    Console.Clear();
+                    Console.WriteLine("How many times do you want to play the game?");
+                    int gameNumber = int.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                    int pcScore = 0;
+                    int myScore = 0;
+
+                    for (int i = 0; i < gameNumber; i++)
+                    {
+                        string computerMove = GetComputerMove();
+                        string playerMove = GetPlayerMove();
+
+                        Console.Clear();
+
+                        Console.WriteLine("Rock.");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("Paper..");
+                        System.Threading.Thread.Sleep(1000);
+                        Console.WriteLine("Scissors...");
+                        System.Threading.Thread.Sleep(1000);
+
+                        Console.Clear();
+
+                        Console.WriteLine("My answer is: " + playerMove);
+                        Console.WriteLine("Computer answer is: " + computerMove);
+
+                        DetermineWinner(playerMove, computerMove, ref myScore, ref pcScore);
+
+                        Console.WriteLine("My Score is: " + myScore);
+                        Console.WriteLine("Pc Score is: " + pcScore);
+                    }
+
+                    Console.WriteLine(GetGameResult(myScore, pcScore));
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You pressed a wrong key. Please try again.");
+                    Start();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a correct value");
+                Start();
+            }
+        }
+
+        private string GetComputerMove()
+        {
+            int randomPc = random.Next(1, 4);
+
+            switch (randomPc)
+            {
+                case 1:
+                    return "rock";
+                case 2:
+                    return "paper";
+                case 3:
+                    return "scissors";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private string GetPlayerMove()
+        {
+            Console.WriteLine("Please choose the move to take: Rock, Paper, or Scissors");
+            string playerMove = Console.ReadLine().ToLower();
+
+            if (playerMove != "rock" && playerMove != "paper" && playerMove != "scissors")
+            {
+                Console.WriteLine("You made a wrong choice");
+                return GetPlayerMove();
+            }
+
+            return playerMove;
+        }
+
+        private void DetermineWinner(string playerMove, string computerMove, ref int myScore, ref int pcScore)
+        {
+            if (playerMove == computerMove)
+            {
+                Console.WriteLine("Tie");
+            }
+            else if ((playerMove == "rock" && computerMove == "scissors") ||
+                     (playerMove == "paper" && computerMove == "rock") ||
+                     (playerMove == "scissors" && computerMove == "paper"))
+            {
+                Console.WriteLine("You won");
+                myScore++;
+            }
+            else
+            {
+                Console.WriteLine("Computer won");
+                pcScore++;
+            }
+        }
+
+        private string GetGameResult(int myScore, int pcScore)
+        {
+            if (myScore > pcScore)
+            {
+                return "You won the game...";
+            }
+            else if (myScore == pcScore)
+            {
+                return "Tie";
+            }
+            else
+            {
+                return "Pc won the game...";
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GameAlgo game = new GameAlgo();
+
+            while (true)
+            {
+                game.Start();
+
+                Console.WriteLine("The game is done. Do you want to start over? Yes/No");
+                string again = Console.ReadLine().ToLower();
+
+                Console.Clear();
+
+                if (again == "no")
+                {
+                    break;
+                }
+                else if (again != "yes")
+                {
+                    Console.WriteLine("Please enter a yes or no answer");
+                }
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
